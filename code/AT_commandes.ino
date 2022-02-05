2d5d444738e469d9f750cdceeb0893d71e02963e
ArduinoProjetArwenEtIlane <94959790+KingExplosionMurderProject@users.noreply.github.com>
2022-02-05 20:18:00 +0100

    Add files via upload

---
+++ code/AT_commandes.ino
@@ -0,0 +1,90 @@
+#include<SoftwareSerial.h>
+#define RX 11 
+#define TX 10
+SoftwareSerial BlueT(RX,TX);
+char recvChar;
+char consigne
+
+
+/* Constantes pour la communication avec le module KT403A */
+const byte COMMAND_BYTE_START = 0x7E;
+const byte COMMAND_BYTE_VERSION = 0xFF;
+const byte COMMAND_BYTE_STOP = 0xEF;
+
+/** Fonction de bas niveau pour envoyer une commande au module KT403A */
+void send_kt_command(byte command, byte data_h, byte data_l, unsigned long cmd_delay) {
+  Serial.write(COMMAND_BYTE_START);
+  Serial.write(COMMAND_BYTE_VERSION);
+  Serial.write((byte) 0x06);
+  Serial.write(command);
+  Serial.write((byte) 0x00);
+  Serial.write(data_h);
+  Serial.write(data_l);
+  Serial.write(COMMAND_BYTE_STOP);
+  // 16-bits checksum is optionnal
+  delay(cmd_delay);
+}
+
+
+void setup(){
+  Serial.begin(9600);
+  delay(500);
+  Serial.println("Bonjour - Pret pour les commandes AT");
+  BlueT.begin(9600);
+  delay(500);}
+
+void loop(){
+  while (BlueT.available()) {
+    consigne=char(BlueT.read());
+    Serial.println(consigne);
+    switch (consigne){
+      case "0":
+        //send_kt_command(0x06, 0, volume, 10)
+        setVolume(0);
+        break;
+      case "1":
+        setVolume(1);
+        break;
+      case "2":
+        setVolume(2);
+        break;
+      case "3":
+        setVolume(3);
+        break;
+      case "4":
+        setVolume(4);
+        break;
+      case "5":
+        setVolume(5);
+        break;
+      case "6":
+        setVolume(6);
+        break;
+      case "7":
+        setVolume(7);
+        break;
+      case "8":
+        setVolume(8);
+        break;
+      case "9":
+        setVolume(9);
+        break;
+
+      case "A":
+        Serial.println("Musique d'après");
+       break;
+      case "B":
+        Serial.println("Musique d'après");
+       break;
+      case "L":
+        Serial.println("lecture mistro");
+        send_kt_command(0x0D, 0, 0, 20);
+        break;
+      case "S":
+        Serial.println("Stop music");
+        send_kt_command(0x16, 0, 0, 20);
+        break;
+    }    
+    
+    }  
+}
