f00966389f193da710fd369879d94c916c9d8abc
ArduinoProjetArwenEtIlane <94959790+KingExplosionMurderProject@users.noreply.github.com>
2022-05-31 19:34:36 +0200

    Update docsignup.php

--- projet ADW/docsignup.php
+++ projet ADW/docsignup.php
@@ -1,50 +1,64 @@
 <?php
+// Ce script vérifie les paramètres envoyés par l'utilisateur
+// et, si ces paramètres sont corrects, réalise le signup puis
+// redirige l'utilisateur vers le script "signin.php",
+// sinon, redirige directement l'utilisateur vers le script
+// "signin.php" avec le bon message d'erreur en paramètre
 	$users = file("users.csv",FILE_IGNORE_NEW_LINES);
 	$login = $_POST[ "login" ];
 	$mdp1 = $_POST[ "password1" ];
 	$mdp2 = $_POST[ "password2" ];
-    $type = $_POST["type"];
+    	$type = $_POST["type"];
 	
-	if ( ! preg_match("/^[a-zA-Z]+$/", $login) ) //si il y a un élément qui n'est pas une lettre, renvoie une erreur
-	{
+	if ( ! preg_match("/^[a-zA-Z]+$/", $login) ){ 	//si il y a un élément qui n'est pas une lettre, renvoie une erreur
 		header("Location: signup.php?badsignup=1");
-		die();
+		die(); 
 	}
 	
 	$found = false;
 	
-	foreach ( $users as $line ) //on va regarder chaque utilisateur déjà inscript
-	{
+	foreach ( $users as $line ){		 //on va regarder chaque utilisateur déjà inscript
 		$a = explode(",",$line);
-		if ( $login == $a[0] )
-		{
-			$found = true;
-			break;
+		if ( $login == $a[0] ){
+			header('Location: signup.php?badsignup=2');		//si l'utilisateur existe déja, renvoie un message d'erreur
+			exit();
 		}
 	}
-	
-	if ( $found ) //si l'utilisateur existe déja, renvoie un message d'erreur
-	{
-		header("Location: signup.php?badsignup=2");
-		die();
-	}
-	
-	if ( strlen( $mdp1 ) < 4 ) // si le mot de passe est trop court, renvoie un message d'erreur
-	{
+
+	if ( strlen( $mdp1 ) < 4 ){		// si le mot de passe est trop court, renvoie un message d'erreur
 		header("Location: signup.php?badsignup=3");
-		die();
+		exit();
 	}
 	
-	if ( $mdp1 != $mdp2 ) // si le mot de passe et sa confirmation sont différents, renvoie une erreur
-	{
+	if ( $mdp1 != $mdp2 ){			// si le mot de passe et sa confirmation sont différents, renvoie une erreur
 		header("Location: signup.php?badsignup=4");
-		die();
+		exit();
 	}
+
 	
-	$users[] = "$login," . md5($mdp1).","."$type,"; // on créé l'utilisateur avec ses info pour le fichier
+	function save_array($f){  //sauvegarder le nouveau fichier
+	    $csv = "";
+	    foreach ($f as $line) { 
+		$tokens = explode(',', $line);
+		$csv .= $tokens[0] . ","  . $tokens[1] . "\n";
+	    }
+	    $csv .= $_POST['login'] . ","  . md5($_POST['password1']) . "\n";
+	    file_put_contents("users.csv", $csv);
+	}
+	save_array($users);
+	$_SESSION["login"] = $login;
+
+	if (isset($_SESSION["goto"])) {header('Location: ' . $_SESSION["goto"]);}
+	else {header('Location: page1.php');}
+	exit();
+
+
+
+
+	/*$users[] = "$login," . md5($mdp1).","."$type,"; // on créé l'utilisateur avec ses info pour le fichier
 	$newcontent = join("\n", $users) . "\n";
 	file_put_contents("users.csv", $newcontent); // on met le nouvel utilisateur dans le fichier
-	header("Location: signin.php");
+	header("Location: signin.php");*/
 
 // rajouter dans le canaux.csv l'utilisateur (listes où il peut aller)
 
