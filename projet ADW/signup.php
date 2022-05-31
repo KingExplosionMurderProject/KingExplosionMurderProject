28dfb24c9c9c801fca39c8f67e36abec0ea0dbba
ArduinoProjetArwenEtIlane <94959790+KingExplosionMurderProject@users.noreply.github.com>
2022-05-31 13:03:52 +0200

    Update signup.php

--- projet ADW/signup.php
+++ projet ADW/signup.php
@@ -1,51 +1,56 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<title>TP 3 - Exo 4</title>
+		<meta name="author" content="DUEE-MOREAU et PELLETIER">
+		<meta name="viewport" content="width=device-width; initial-scale=1.0">
+	
+	</head>
+	<body>
+		<h2>Inscription</h2>
+
+		<form action="dosignup.php" method="post">
+			Choisissez votre login (uniquement des lettres minuscules ou majuscules)
+			<br>
+			<input type="text" name="login">
+			<br><br>
+			Choisissez votre mot de passe (minimum 4 caractères)
+			<br>
+			<input type="password" name="password1">
+			<br>
+			Répétez votre mot de passe
+			<br>
+			<input type="password" name="password2">
+			<br><br>
+            Sélectionner votre typer
+            <br>
+            <select name="type">
+                <option value="eau">eau</option>
+                <option value="feu">feu</option>
+                <option value="plante">plante</option>
+				<option value="électrique">électrique</option>
+				<option value="fée">fée</option>
+            </select>
+            <br><br>
+			<input type="submit" value="S'inscrire">
+			<input type="reset" value="Annuler">
+		</form>
 <?php
-	$users = file("users.csv",FILE_IGNORE_NEW_LINES);
-	$login = $_POST[ "login" ];
-	$mdp1 = $_POST[ "password1" ];
-	$mdp2 = $_POST[ "password2" ];
-    $type = $_POST["type"];
-	
-	if ( ! preg_match("/^[a-zA-Z]+$/", $login) ) //si il y a un élément qui n'est pas une lettre, renvoie une erreur
+	if ( isset( $_GET[ "badsignup" ] ) )
 	{
-		header("Location: signup.php?badsignup=1");
-		die();
-	}
-	
-	$found = false;
-	
-	foreach ( $users as $line ) //on va regarder chaque utilisateur déjà inscript
-	{
-		$a = explode(",",$line);
-		if ( $login == $a[0] )
+		switch ( $_GET[ "badsignup" ] )
 		{
-			$found = true;
-			break;
+			case "1": $msg = "le login ne contient pas que des lettres"; break;
+			case "2": $msg = "le login est déjà utilisé"; break;
+			case "3": $msg = "le mot de passe a moins de 4 caractères"; break;
+			default:  $msg = "le mot de passe et la vérification sont différents";
 		}
+		echo "<h2>$msg</h2>";
 	}
-	
-	if ( $found ) //si l'utilisateur existe déja, renvoie un message d'erreur
-	{
-		header("Location: signup.php?badsignup=2");
-		die();
-	}
-	
-	if ( strlen( $mdp1 ) < 4 ) // si le mot de passe est trop court, renvoie un message d'erreur
-	{
-		header("Location: signup.php?badsignup=3");
-		die();
-	}
-	
-	if ( $mdp1 != $mdp2 ) // si le mot de passe et sa confirmation sont différents, renvoie une erreur
-	{
-		header("Location: signup.php?badsignup=4");
-		die();
-	}
-	
-	$users[] = "$login," . md5($mdp1).","."$type,"; // on créé l'utilisateur avec ses info pour le fichier
-	$newcontent = join("\n", $users) . "\n";
-	file_put_contents("users.csv", $newcontent); // on met le nouvel utilisateur dans le fichier
-	header("Location: signin.php");
-
-// rajouter dans le canaux.csv l'utilisateur (listes où il peut aller)
-
 ?>
+	</body>
+</html>
+
+
+
