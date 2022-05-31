895eca8b47859853db849ebb4b81d373b854ac53
ArduinoProjetArwenEtIlane <94959790+KingExplosionMurderProject@users.noreply.github.com>
2022-05-31 13:35:52 +0200

    Update signup.php

--- projet ADW/signup.php
+++ projet ADW/signup.php
@@ -5,37 +5,40 @@
 		<title>Pokechat</title>
 		<meta name="author" content="DUEE-MOREAU et PELLETIER">
 		<meta name="viewport" content="width=device-width; initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="feuille_css.css" />
 	
 	</head>
 	<body>
 		<h2>Inscription</h2>
-
-		<form action="dosignup.php" method="post">
-			Choisissez votre login (uniquement des lettres minuscules ou majuscules)
-			<br>
-			<input type="text" name="login">
-			<br><br>
-			Choisissez votre mot de passe (minimum 4 caractères)
-			<br>
-			<input type="password" name="password1">
-			<br>
-			Répétez votre mot de passe
-			<br>
-			<input type="password" name="password2">
-			<br><br>
-            Sélectionner votre typer
-            <br>
-            <select name="type">
-                <option value="eau">eau</option>
-                <option value="feu">feu</option>
-                <option value="plante">plante</option>
-		<option value="électrique">électrique</option>
-		<option value="fée">fée</option>
-            </select>
-            <br><br>
-			<input type="submit" value="S'inscrire">
-			<input type="reset" value="Annuler">
-		</form>
+		<div id="inscription">
+			<form action="dosignup.php" method="post">
+				Choisissez votre login (uniquement des lettres minuscules ou majuscules)
+				<br>
+				<input type="text" name="login">
+				<br><br>
+				Choisissez votre mot de passe (minimum 4 caractères)
+				<br>
+				<input type="password" name="password1">
+				<br>
+				Répétez votre mot de passe
+				<br>
+				<input type="password" name="password2">
+				<br><br>
+	            Sélectionner votre type
+	            <br>
+	            <select name="type">
+	                <option value="eau">eau</option>
+	                <option value="feu">feu</option>
+	                <option value="plante">plante</option>
+	                <option value="électrique">électrique</option>
+					<option value="fée">fée</option>
+	            </select>
+	            <br><br>
+				<input type="submit" value="S'inscrire">
+				<input type="reset" value="Annuler">
+			</form>
+		</div>
+		<div class="center"> <img id="inscription" src="images/pokeball.png"/> </div>
 <?php
 	if ( isset( $_GET[ "badsignup" ] ) )
 	{
@@ -51,6 +54,3 @@
 ?>
 	</body>
 </html>
-
-
-
