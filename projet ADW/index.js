a313b3317c4647b6d0a88587395690071723f808
ArduinoProjetArwenEtIlane <94959790+KingExplosionMurderProject@users.noreply.github.com>
2022-05-31 10:34:52 +0200

    Update index.js

--- projet ADW/index.js
+++ projet ADW/index.js
@@ -1,10 +1,14 @@
-
 window.onload = function () {
 	//quand on clique il vérifie si on est déja connecter sinon il propose de se loguer.
 
-	
+	var miniature = document.querySelectorAll(".miniature");
+	for(let i = 0; i < 3; i++){
+		miniature[i].onclick= go_chat; 
+	}
+
+	function go_chat(){
+		window.location.href = "formulaire.php";
+		console.log("go chatter");
+	}
+
 };
-
-
-//pas sur d'utiliser cette feuille
-
