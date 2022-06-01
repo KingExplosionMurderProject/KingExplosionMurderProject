e38bbf8000b26a58379134fa49161afc96eff3e0
ArduinoProjetArwenEtIlane <94959790+KingExplosionMurderProject@users.noreply.github.com>
2022-06-01 22:26:22 +0200

    Update formulaire.js

--- projet ADW/formulaire.js
+++ projet ADW/formulaire.js
@@ -11,8 +11,21 @@ window.onload = function () {
 		leNomDuChannel.style.visibility = "visible";
 		valider.style.visibility = "visible";
 	}
+
+	function changerChannel(){
+
+		let nc = document.getElementById("name_chanel");
+		nc.innerHTML = "Nous sommes à "+this.innerHTML; //this = channel[i]
+	}
+
+
 	let button = document.getElementById("Nouveau channel");
 	button.onclick = nomChannel;
+
+	var channel = document.querySelectorAll(".region");
+	for(i=0;i<channel.length;i++){
+		channel[i].onclick= changerChannel; 
+	}
 
 
 };
