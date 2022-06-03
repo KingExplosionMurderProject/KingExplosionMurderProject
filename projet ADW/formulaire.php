cec4ca620493fde4aade95db8fc5861bac3a98cc
ArduinoProjetArwenEtIlane <94959790+KingExplosionMurderProject@users.noreply.github.com>
2022-06-03 13:27:47 +0200

    Update formulaire.php

--- projet ADW/formulaire.php
+++ projet ADW/formulaire.php
@@ -10,6 +10,8 @@
 		exit();
 	}
 	else{$login=$_SESSION["login"];}
+
+	$type="energies\électrique.png";
 	
 
 ?>
@@ -18,28 +20,34 @@
 <head>
 	<title>Pokechat</title>
     <meta name="author" content="Duee-Moreau et Pelletier">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="feuille_css.css" />
-
-	<!-- <script type="text/javascript" src="js/simpleajax.js"></script> 
-	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />-->
 	<script src="formulaire.js"></script>
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
 </head>
 <body>
 
-	<header>
+	<header>										<!-- ENTETE -->
 		<img src="images/pokeball.png"/>
         <h1>POKECHAT</h1>
         <img src="images/pokeball.png"/>
     </header>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 
     <div id="main"> <!-- gros contenaire-->
-		<div class="list"><!--les autres noms de salons-->
-			<div class="boite_user">
+
+
+		<aside class="list">						<!--les autres noms de salons-->
+
+			<div class="boite_user">						<!-- Boite utilisateur -->
 				Utilisateur : <strong> <?php echo $login; ?> </strong>
 				<br>
+				<img src="<?php echo $type;?>" id="boite_user"/>
 				<a href="signout.php">déconnexion</a>
 			</div>
+
 			<form action="channelsignup.php" method="post">
 				<h2>Canaux</h2>
 
@@ -50,43 +58,73 @@
 				
 				<br>
 				<br>
-				<?php
-				foreach(file("fichiersCSV/canaux.csv", FILE_IGNORE_NEW_LINES) as $line){
-					$tokens = explode(',',$line);
-					echo "<div class='region'>";
-					echo "<div class='nomRegion'>".$tokens[0]."</div>";
-					echo "<div class='parametre'><img class='molette' src='images/molette.png' window.onload=param()></div>";	//elles serviront à sortir du channel, voir le créateur
-					echo "</div>";
-					echo "<br>";
-				}
-				?>
+
+				<div id="spinner">Spinner</div>
+				<ul id="list_canaux"></ul>
+
+
+				<!-- Modal -->
+				<div class="modal fade" id="parametresModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+				  <div class="modal-dialog">
+				    <div class="modal-content">
+				      <div class="modal-header">
+				        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+				        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+				      </div>
+				      <div class="modal-body">
+				        ...
+				      </div>
+				      <div class="modal-footer">
+				        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+				        <button type="button" id="quitterChannel" class="btn btn-primary" data-bs-dismiss="modal">Quitter channel</button>
+				      </div>
+				    </div>
+				  </div>
+				</div>
+
+
 			</form>
-		</div> 
+		</aside> 
+
+
+
 		<div id="chat"> <!--discussion principale-->
 			<div id="place">
-				<h2>Blabla 2</h2>
 				<span id="name_chanel">blablabla</span>
 				<div id="write">
 
+
 				<!--ajouté par arwen-->
 					<p class="chat">
+						<!-- <div class="boite_parametre" style="display:none;">			ajouté par ilane 	
+							<?php /*
+							echo "<h2>Paramètre du chat "."</h2>"; //savoir quel molette c'est de quel channel
+							echo "<h6>créé par </h6>"; //récuperer le créateur du channel
+							echo "<input type='button' id='Inviter channel' value='Inviter'>";
+							echo "<input type='button' id='Quitter channel' value='Quitter channel'>";*/
+							?>
+						</div> -->
+						<div class="discu">
 
-			<?php 
-			
-				if (isset($_POST['message']) && $_POST['message'] !== ""){
-					newMessage("fichiersCSV\chat2.csv");
-				} 
-				else {
-					printChat("fichiersCSV\chat2.csv");
-				}
-				
-			?>
+						<?php 
+						
+							if (isset($_POST['message']) && $_POST['message'] !== ""){
+								newMessage("fichiersCSV\chat2.csv");
+							} 
+							else {
+								printChat("fichiersCSV\chat2.csv");
+							}
+							
+						?>
+						</div>
 
-		<p>
+					</p>
 
 
 			<?php
 			//partie écriture des messages
+			// une balise <p> par message
+			// overflow-y: auto;
 
 				ecritMess("fichiersCSV\chat2.csv"); //boite pour écire les messages
 
