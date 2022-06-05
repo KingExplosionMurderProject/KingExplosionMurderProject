a967f590bc1b6ae87722e7c9a95b0aa4f5baeceb
ArduinoProjetArwenEtIlane <94959790+KingExplosionMurderProject@users.noreply.github.com>
2022-06-05 18:27:26 +0200

    Create loadchat.php

---
+++ projet ADW/loadchat.php
@@ -0,0 +1,10 @@
+<?php
+    $mess = file("fichiersCSV\chat2.csv");
+    $num = count($mess);
+    $affiche = ""; //string avec les messages qui vont être affichés      
+    for ($j=0;$j<$num;$j++){
+        $affiche .= "<p>".$mess[$j]."</p>";
+    }
+
+    echo $affiche;
+?>
