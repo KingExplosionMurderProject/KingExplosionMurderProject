29f286984337d51bee02bc422bc91c97cb62f80c
ArduinoProjetArwenEtIlane <94959790+KingExplosionMurderProject@users.noreply.github.com>
2022-06-05 18:29:47 +0200

    Update chat.php

--- projet ADW/chat.php
+++ projet ADW/chat.php
@@ -2,57 +2,26 @@
 
 //le dernier message envoyé est automatiquement gardé, ce qui pose problème
 
-
-    // session start permet de récupérer certaines info 
     function printChat($fichier){
+        //appelé une seule fois, lors de la première connection
+        //avavnt l'envoie des messages
         //permet d'afficher les 8 derniers messages de la chatroom
         $mess = file($fichier);
-
         $num = count($mess);
-
-        $affiche = ""; //string avec les messages qui vont être affichés
-
-        //on veut afficher au maximum 8 messages à la fois
-
-        if ($num<=8){
-            foreach($mess as $message){
-                $affiche .= "<p>".$message."<p>";
-            }
+        $affiche = ""; //string avec les messages qui vont être affichés      
+        for ($j=0;$j<$num;$j++){
+            $affiche .= "<p>".$mess[$j]."</p>";
         }
-        else {
-            for ($j=$num-8;$j<$num;$j++){
-                $affiche .= "<p>".$mess[$j]."<p>";
-            }
-        }
+
         echo $affiche;
 
-
-
-
     }
 
-
-    function newMessage($fichier){
-        //écrit le nouveau message et actualise le chat
-        $message = "\n".$_POST['message'];
-    header("Location: formulaire.php"); //permet que si on actualise la page juste apres avoir envoyé le message, celui*ci n'est pas renvoyé
-        // on met les nouveaux messages ensembles
-        $chat = file($fichier);
-        array_push($chat,$message);
-        
-        
-        file_put_contents($fichier,$chat);
-
-        printChat($fichier);
-    
-    }
-
-    function ecritMess($fichier){
+    function ecritMess(){
         // on fait une fonction comme ça on l'apelle suivant le salon ouvert
-        $input = "<form id='newmess' method='post'>";
-        $input .= "<br>".'<input type="text" name="message" id="message" autocomplete="off" placeholder="Discutez avec vos amis">'."<br>";
-        $input .= "<br>".'<input type="submit" value="envoyer" id="submit">'."<br>";
-        $input .= "</form>";
+        //changer pour du textarea
+        $input = "<br>".'<textarea name="message" id="message" autocomplete="off" placeholder="Discutez avec vos amis">'.'</textarea>'."<br>";
+        $input .= "<br>".' <input type="submit" id="envoyer" value="envoyer">'."<br>";
         echo $input;
 
     }
