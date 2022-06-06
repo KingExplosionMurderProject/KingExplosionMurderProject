638d7a4dc795f62ae76327f9ec46690085d5f833
ArduinoProjetArwenEtIlane <94959790+KingExplosionMurderProject@users.noreply.github.com>
2022-06-06 13:44:09 +0200

    Update formulaire.js

--- projet ADW/formulaire.js
+++ projet ADW/formulaire.js
@@ -3,8 +3,6 @@ window.onload = function () {
 	//import readTextFile() from utils.js;
 	// Recuperer liste des canaux
 	//https://developer.mozilla.org/fr/docs/Web/JavaScript/Guide/Modules
-	
-	console.log("hello world");
 
 	var canaux = null;
 	var currentChannel = null;
@@ -56,7 +54,7 @@ window.onload = function () {
 				return col.split(',');
 			});
 			// liste de canaux recupere
-			console.log(canaux);
+			//console.log(canaux);
 		}, (err) => {
 			// Error
 			console.log(err);
@@ -70,10 +68,11 @@ window.onload = function () {
 			refreshChannels();
 		});
 
-
+		console.log("etape 1");
 		readTextFile('fichiersCSV/users.csv').then((res) => {
 			// Success
-			users = res.split("\r\n");
+
+			users = res.split("\n");
 			users = users.filter((line) => {
 				// For each, execution pour chaque line
 				return (line && line.length > 0);
@@ -82,7 +81,7 @@ window.onload = function () {
 				return col.split(',');
 			});
 			// liste de users recupere
-			//console.log(users);
+			console.log(users);
 		}, (err) => {
 			// Error
 			console.log(err);
@@ -91,7 +90,13 @@ window.onload = function () {
 		}).finally(() => {
 			let i=0;
 			users.forEach((user)=> {
+
+
+
 				if(user[0]==login){
+
+
+
 					whichType(user);
 				}
 				i++;
@@ -146,7 +151,8 @@ window.onload = function () {
 	}
 
 	function whichType(user){
-		//let img = document.getElementById("energiepp");
+		//console.log(user[2]);
+		console.log(user);
 		document.getElementById("energiepp").src = "energies\\" + user[2] +".png";
 	}
 
@@ -200,5 +206,194 @@ window.onload = function () {
 		console.log(currentChannel);
 		// Executer le code pour supprimer l'utilisateur du canal
 	});
+
+
+
+
+
+//recherche chat------------------------------------------------------------------------------------------------------
+
+function autocomplete(inp, arr) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  var currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      /*close any already open lists of autocompleted values*/
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      /*create a DIV element that will contain the items (values):*/
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(a);
+      /*for each item in the array...*/
+      for (i = 0; i < arr.length; i++) {
+        /*check if the item starts with the same letters as the text field value:*/
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+              b.addEventListener("click", function(e) {
+              /*insert the value for the autocomplete text field:*/
+              inp.value = this.getElementsByTagName("input")[0].value;
+              /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+      x[i].parentNode.removeChild(x[i]);
+    }
+  }
+}
+/*execute a function when someone clicks in the document:*/
+document.addEventListener("click", function (e) {
+    closeAllLists(e.target);
+});
+} 
+
+autocomplete(document.getElementById("RuC"), canaux);
+
+
+
+
+// partie du chat-----------------------------------------------------------------------------------------------------
+
+	setInterval(loadChat,1000);
+
+	let sub = document.getElementById("envoyer");
+    sub.addEventListener('click', event => {
+        message();})
+
+
+    function message(){
+		
+
+		//on ajoute le message aux anciens
+		add();
+
+		//on sauvegarde tout ça
+		saveChat();
+
+
+		//je sais que c'est pas forcément la meilleur manière ou propre
+		function add(){
+			//on récupère le nouveau message
+			let message = document.getElementById('message').value+"\n";
+
+			//on récupère la boite où sont affiché les messages
+			let chatroom = document.getElementById('emplacement');
+
+			//on créé le nouvel élément message et on l'ajoute
+			let toAdd = document.createElement("p");
+			toAdd.innerHTML = message;
+			chatroom.appendChild(toAdd);
+			document.getElementById('message').value = "";
+			
+
+		}
+		
+		
+        
+    }
+
+
+//fonctions pour save le chat
+
+function saveChat(){
+	let array = []; 
+
+	//on récupère l'intérieur du chat
+	let chat = document.getElementById('emplacement').innerHTML;
+	
+	//on enlève les balises <p>
+	array = chat.split("<p>");
+	let finalChat = [];
+	let message;
+	let number = array.length;
+	for(let i =1;i<number;i++){
+		message = array[i].split('</p>');
+		finalChat.push(message[0]);
+
+	}
+	new simpleAjax("savechat.php","post","chat="+JSON.stringify(finalChat),load,fail);
+
+	
+
+	
+}
+
+//fonction pour load le chat
+function loadChat(){
+	new simpleAjax("loadchat.php","post","",load,fail);
+}
+
+
+function load(request){
+	let chat = document.getElementById('emplacement');
+	chat.innerHTML = request.responseText;
+}
+function fail() {
+	alert("problème");
+}
+
 
 };
